bf22f6df24d3872fcb361b7c2610cdafb2e14063
Audias22 <rguevaram1@miumg.edu.gt>
2026-08-12 23:32:49 -0600

    panel: mostrar cantidad al final de cada barra en tipos notariales

--- panel-web/src/components/charts/BarChart.jsx
+++ panel-web/src/components/charts/BarChart.jsx
@@ -1,6 +1,7 @@
 import {
   BarChart as ReBarChart,
   Bar,
+  LabelList,
   XAxis,
   YAxis,
   CartesianGrid,
@@ -11,7 +12,7 @@ import {
 function BarChart({ datos, dataKeyValor = 'cantidad', dataKeyNombre = 'nombre', altura = 280 }) {
   return (
     <ResponsiveContainer width="100%" height={altura}>
-      <ReBarChart data={datos} layout="vertical" margin={{ top: 8, right: 24, left: 8, bottom: 0 }}>
+      <ReBarChart data={datos} layout="vertical" margin={{ top: 8, right: 40, left: 8, bottom: 0 }}>
         <CartesianGrid strokeDasharray="3 3" stroke="#E8E4DF" horizontal={false} />
         <XAxis type="number" tick={{ fontSize: 12, fill: '#8A8580' }} allowDecimals={false} />
         <YAxis
@@ -21,7 +22,21 @@ function BarChart({ datos, dataKeyValor = 'cantidad', dataKeyNombre = 'nombre', 
           width={130}
         />
         <Tooltip cursor={{ fill: 'rgba(212, 168, 83, 0.15)' }} />
-        <Bar dataKey={dataKeyValor} fill="#D4A853" radius={[0, 4, 4, 0]} barSize={22} />
+        <Bar
+          dataKey={dataKeyValor}
+          fill="#D4A853"
+          radius={[0, 4, 4, 0]}
+          barSize={22}
+          minPointSize={1}
+        >
+          <LabelList
+            dataKey={dataKeyValor}
+            position="right"
+            fill="#1B2A4A"
+            fontSize={12}
+            fontWeight={600}
+          />
+        </Bar>
       </ReBarChart>
     </ResponsiveContainer>
   )
